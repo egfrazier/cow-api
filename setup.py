b9c75d1b3e7c9a89cfc66cd345ea6448eef1ecc8
Elizabeth Frazier <liz@egfrazier.com>
2018-07-06 04:56:20 -0700

    Adding MANIFEST.in and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='cowapi',
+	version='0.1.0',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'flask',
+		'atomicwrites',
+		'attrs',
+		'configparser',
+		'coverage',
+		'pluggy',
+		'py',
+		'PyMySQL',
+		'pytest',
+		'six'
+	],
+)
